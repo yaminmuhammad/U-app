ba88e933d7249a85db6f0250caeb5fe527539e39
yaminmuhammad <1910631170108@student.unsika.ac.id>
2022-05-14 13:58:40 +0700

    creating an image card component

---
+++ src/components/ImageCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class ImageCard extends React.Component {
+    render() {
+        const { urls, description } = this.props.image;
+
+        return (
+            <div>
+                <img
+                    src={urls.regular}
+                    alt={description}
+                />
+            </div>
+        );
+    }
+}
+
+export default ImageCard;
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import './ImageList.css'
+import ImageCard from './ImageCard';
 
 
 const ImageList = (props) => {
     const images = props.images.map((image) => {
         // eslint-disable-next-line jsx-a11y/alt-text
-        return <img src={image.urls.regular} />
+        return <ImageCard key={image.id} image={image} />
     });
-    return <div>{images}</div>;
+    return <div className='image-list'>{images}</div>;
 }
 
 export default ImageList;
