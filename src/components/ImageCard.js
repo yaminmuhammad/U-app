0dd42c68f3ad101757b541a7a2d86e27a88740b1
yaminmuhammad <1910631170108@student.unsika.ac.id>
2022-05-14 14:45:27 +0700

    callbacks on image load

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,26 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
+        console.log(this.imageRef.current.clientHeight);
+    }
+
     render() {
         const { urls, description } = this.props.image;
-
         return (
             <div>
                 <img
+                    ref={this.imageRef}
                     src={urls.regular}
                     alt={description}
                 />
